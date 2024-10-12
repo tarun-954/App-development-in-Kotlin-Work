0af8a8005bd061283c8bc0aee534331de40fde56
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-10-12 22:42:02 +0530

    Practice XMl and kotlin

---
+++ app/src/main/java/com/example/applocalize/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.applocalize
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import android.util.Log
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge() // Enables edge-to-edge layout
+        setContentView(R.layout.activity_main)
+
+        // Find the button in the layout
+        val showSnackbarButton: Button = findViewById(R.id.btnsnackbar1)
+
+        // Set up an OnClickListener for the button
+        showSnackbarButton.setOnClickListener { view ->
+            // Create and show the Snackbar
+            val snackbar = Snackbar.make(view, "This is my Snackbar message", Snackbar.LENGTH_LONG)
+            snackbar.setAction("Close") {
+                Log.e("MainActivity", "Snackbar closed")
+            }
+            snackbar.setBackgroundTint(Color.BLUE);
+            snackbar.show()
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/applocalize/MainActivity2.kt
@@ -0,0 +1,75 @@
+    package com.example.applocalize
+
+    import android.annotation.SuppressLint
+    import android.os.Bundle
+    import androidx.activity.enableEdgeToEdge
+    import androidx.appcompat.app.AppCompatActivity
+    import androidx.core.view.ViewCompat
+    import androidx.core.view.WindowInsetsCompat
+    import android.widget.Button
+    import android.content.Intent
+    import android.net.Uri
+
+    class MainActivity2 : AppCompatActivity() {
+        @SuppressLint("MissingInflatedId")
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            enableEdgeToEdge()
+            setContentView(R.layout.activity_main2)
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
+
+
+    //        this is the code for explecit
+
+            val explicit_button=findViewById<Button>(R.id.button3);
+            explicit_button.setOnClickListener{
+                val intent=Intent(this,MainActivity::class.java)
+                startActivity(intent)
+
+            }
+
+    //        code for impicit
+    //        action view
+    //        action dial
+    //        action send  -> send the message to somebody
+    //        action setting
+
+
+
+
+
+
+    //code for implicit using action view
+var implicit_btn=findViewById<Button>(R.id.button4)
+            implicit_btn.setOnClickListener{
+                var url="https://www.youtube.com"
+                val intent=Intent(Intent.ACTION_VIEW)
+                intent.data= Uri.parse(url)
+                startActivity(intent);
+            }
+
+
+//           impicit using action dial
+var implicitDialar=findViewById<Button>(R.id.button5)
+            implicitDialar.setOnClickListener{
+                val phonenumber="+91 9805394341"
+                val intent=Intent(Intent.ACTION_DIAL)
+                intent.data=Uri.parse("tel:$phonenumber");
+                startActivity(intent);
+            }
+
+val sendsmsbutton=findViewById<Button>(R.id.button6)
+            sendsmsbutton.setOnClickListener{
+                val shareIntent=Intent(Intent.ACTION_SEND)
+//                shareIntent
+            }
+
+
+
+
+        }
+    }
